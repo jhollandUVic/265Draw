7856ced795cc4ee67b3f88240cde76688e5f07f4
John Holland <jholland@uvic.ca>
2017-03-29 16:55:58 -0700

    Commit
    sync master

--- sierpinski.sh
+++ sierpinski.sh
@@ -42,31 +42,31 @@ fi
 scale_to_a_third=.3333
 
 echo	"*"
-echo	"* Create a Recursive Sierpinski Carpet with ${recursions} Levels"
+echo	"* Create a Recursive Sierpinski carpet line file with ${recursions} Levels"
 CMD="python generate_carpet.py ${recursions} ${col_theme} > carpet_${recursions}.txt"
 echo "${CMD}"
 eval "${CMD}"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo	"*"
-echo	"* Create colour svg file from base carpet"
+echo	"* Create colour svg file from base carpet line file"
 CMD="python lines_to_svg_colour.py < carpet_${recursions}.txt > carpet_${recursions}.svg"
 echo "${CMD}"
 eval "${CMD}"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo	"*"
-echo	"* Scale carpet lines down to fit in central ninth of canvas"
+echo	"* Scale carpet line file down to fit in central ninth of canvas"
 CMD="python rotate_scale_translate.py -f ${scale_to_a_third} < carpet_${recursions}.txt > carpet_${recursions}S.txt"
 echo "${CMD}"
 eval "${CMD}"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo	"*"
-echo	"* Build tile patterns from scaled file"
+echo	"* Build tile patterns from scaled line file"
 CMD="python transform_carpet.py ${pattern} < carpet_${recursions}S.txt > carpet_${recursions}ST.txt"
 echo "${CMD}"
 eval "${CMD}"
 rc=$?; if [[ $rc != 0 ]]; then exit $rc; fi
 echo	"*"
-echo "*	Create colour svg file from tiled carpet"
+echo "* Create colour svg file from tiled carpet line file"
 CMD="python lines_to_svg_colour.py < carpet_${recursions}ST.txt > carpet_${recursions}ST.svg"
 echo "${CMD}"
 eval "${CMD}"
